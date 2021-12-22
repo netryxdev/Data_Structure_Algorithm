14c37276fe757ab77ef345487fa179d1291bab94
netryxdev <adrianoneto2501@gmail.com>
2021-12-21 21:35:49 -0300

    first commit

---
+++ 1_Programacao_basica_em_python/ex2_conditionals.py
@@ -0,0 +1,13 @@
+children = 0 <= 12
+teen = 13 <= 17
+adult = 18 <= 150 
+
+age = 30
+
+
+if age < 13: 
+    print('É uma criança.')
+elif age > 13 | age <=17 : 
+    print('Adolescente.')
+else :
+   print('É um adulto.')
