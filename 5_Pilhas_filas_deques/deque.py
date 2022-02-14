2c0be66c5cd052262f053a83459f8072739034c2
netryxdev <adrianoneto2501@gmail.com>
2022-02-14 03:35:26 -0300

    listas

--- 5_Pilhas_filas_deques/deque.py
+++ 5_Pilhas_filas_deques/deque.py
@@ -45,3 +45,61 @@ class Deque:
             self.final = 0
         else:
             self.final += 1
+        self.valores[self.final] = valor
+
+    def excluir_inicio(self):
+        if self.__deque_vazio():
+            print('O deque já está vazio')
+            return
+
+        # Possui somente um elemento
+        if self.inicio == self.final:
+            self.inicio = -1
+            self.final = -1
+        else: 
+            # Volta para a posição inicial
+            if self.inicio == self.capacidade -1:
+                self.inicio = 0
+            else:
+                # Incrementar o início para remover o início atual
+                self.inicio += 1
+
+    def excluir_final(self):
+        if self.__deque_vazio():
+            print('O deque já está vazio')
+            return
+
+        if self.inicio == self.final:
+            self.inicio = -1
+            self.final = -1
+        elif self.inicio == 0:
+            self.final = self.capacidade -1
+        else:
+            self.final -= 1
+
+    def get_inicio(self):
+        if self.__deque_vazio():
+            print('O deque já está vazio')
+            return
+        
+        return self.valores[self.inicio]
+
+    def get_final(self):
+        if self.__deque_vazio() or self.final < 0:
+            print('O deque já está vazio')
+            return
+
+        return self.valores[self.final]
+
+deque = Deque(5)
+deque.insere_final(5)
+deque.insere_final(10)
+deque.insere_inicio(3)
+deque.insere_inicio(2)
+deque.insere_final(11)
+
+deque.insere_inicio(43)
+
+deque.excluir_inicio()
+deque.excluir_final()
+
