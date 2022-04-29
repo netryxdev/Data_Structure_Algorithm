349bfd1f19aa2e4df1f816541c1e61b74c77fecb
Neto <adrianoneto2501@gmail.com>
2022-04-29 19:37:00 -0300

    arvore binaria insercao e visualizacao

--- 9_Árvores/1_arvoreBinaria_implementacao_1.py
+++ 9_Árvores/1_arvoreBinaria_implementacao_1.py
@@ -12,3 +12,40 @@ class No:
 class ArvoreBinariaBusca:
     def __init__(self):
         self.raiz = None
+
+    def inserir(self, valor):
+        novo = No(valor)
+        # Se a árvore estiver vazia
+        if self.raiz == None:
+            self.raiz = novo
+        else:
+            atual = self.raiz
+            while True:
+                pai = atual
+                # Esquerda
+                if valor < atual.valor:
+                    atual = atual.esquerda
+                    if atual == None:
+                        pai.esquerda = novo
+                        return
+                # Direita
+                else:
+                    atual = atual.direita
+                    if atual == None:
+                        pai.direita = novo
+                        return
+
+### INSERÇÃO e VISUALIZAÇÃO
+arvore = ArvoreBinariaBusca()
+arvore.inserir(53)
+arvore.inserir(30)
+arvore.inserir(14)
+arvore.inserir(39)
+arvore.inserir(9)
+arvore.inserir(23)
+arvore.inserir(34)
+arvore.inserir(49)
+arvore.inserir(72)
+arvore.inserir(61)
+arvore.inserir(84)
+arvore.inserir(79)
